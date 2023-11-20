bb3ae5ca7083afcab56f8242b81f6a2275e1fb76
prathmeshpote99 <poteprathmesh@gmail.com>
2023-11-20 17:03:14 +0530

    feat: host name change

--- src/app/helpers/route/authPrevent.jsx
+++ src/app/helpers/route/authPrevent.jsx
@@ -11,7 +11,7 @@ const AuthPrevented = ({ component: Component, ...rest }) => (
       ) : (
         <Redirect
           to={{
-            pathname: "/dashboard",
+            pathname: "/home",
             state: { from: props.location },
           }}
         />
